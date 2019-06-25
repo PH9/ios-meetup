94b1c23a33b845df8635750f666f56ec83009857
Wasith Theerapattrathamrong <phai@live.com>
2019-06-25 10:36:01 +0700

    Add Alamofire to project

--- iOS/ViewController.swift
+++ iOS/ViewController.swift
@@ -1,20 +1,11 @@
-//
-//  ViewController.swift
-//  iOS
-//
-//  Created by Wasith Theerapattrathamrong on 25/6/2562 BE.
-//  Copyright © 2562 Kasikorn Business-Technology Group. All rights reserved.
-//
-
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
