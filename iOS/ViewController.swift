4a98f3e237e965af065412423d2e1344d10e0eb7
Wasith Theerapattrathamrong <phai@live.com>
2019-06-25 10:08:16 +0700

    Initial Commit

---
+++ iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Wasith Theerapattrathamrong on 25/6/2562 BE.
+//  Copyright © 2562 Kasikorn Business-Technology Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
